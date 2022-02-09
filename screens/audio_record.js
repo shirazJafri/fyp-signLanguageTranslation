09556f0edb0a087e811c5cfb8c6da17a9cebc843
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2022-02-10 00:52:41 +0500

    Add files via upload

---
+++ screens/audio_record.js
@@ -0,0 +1,166 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import Voice from '@react-native-voice/voice';
+// import SpeechToText from 'react-native-google-speech-to-text';
+
+// TODO: What to do with the module?
+
+const Audio_record = ({navigation}) => {
+
+  const [result, setResult] = useState('')
+  const [isLoading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStartHandler;
+    Voice.onSpeechEnd = onSpeechEndHandler;
+    Voice.onSpeechResults = onSpeechResultsHandler;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    }
+  }, [])
+//   const speechToTextHandler = async () => {
+
+//     let speechToTextData = null;
+//         try {
+//             speechToTextData = await SpeechToText.startSpeech('Try saying something', 'en_IN');
+//             console.log('speechToTextData: ', speechToTextData);
+//             setResult(text)
+
+//         } catch (error) {
+//             console.log('error: ', error);
+//         }
+// }
+  const onSpeechStartHandler = (e) => {
+    console.log("start handler==>>>", e)
+  }
+  const onSpeechEndHandler = (e) => {
+    setLoading(false)
+    console.log("stop handler", e)
+  }
+
+  const onSpeechResultsHandler = (e) => {
+    let text = e.value[0]
+    setResult(text)
+    console.log("speech result handler", e)
+  }
+
+  const startRecording = async () => {
+    setLoading(true)
+    try {
+      await Voice.start('en-Us')
+    } catch (error) {
+      console.log("error raised", error)
+    }
+  }
+
+  const stopRecording = async () => {
+    try {
+      setLoading(false)
+      await Voice.stop()
+    } catch (error) {
+      console.log("error raised", error)
+    }
+  }
+  const clear = () =>{
+    setResult("")
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Text style={styles.headingText}>Sign Language Translator</Text>
+        <View style={styles.textInputStyle}>
+          <TextInput
+            value={result}
+            placeholder="Enter your text or record it."
+            style={{width:250, maxHeight: 300}}
+            multiline
+            onChangeText={text => setResult(text)}
+          />
+          {isLoading ? <ActivityIndicator size="large" color="red" />
+
+            :
+            
+            <TouchableOpacity
+              onPress={startRecording}
+            >
+              <Image
+                source={{ uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png' }}
+                style={{ width: 25, height: 25 }}
+              />
+            </TouchableOpacity>}
+        </View>
+
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+            marginTop: 24,
+            backgroundColor: 'red',
+            padding: 8,
+            borderRadius: 4
+          }}
+          onPress={stopRecording}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>Stop</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+            marginTop: 24,
+            backgroundColor: 'red',
+            padding: 8,
+            borderRadius: 4
+          }}
+          onPress={clear}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>clear</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+            marginTop: 24,
+            backgroundColor: 'red',
+            padding: 8,
+            borderRadius: 4
+          }}
+          onPress={
+            () => navigation.navigate('Video_Screen',{res:result})
+          }
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>Translate</Text>
+          </TouchableOpacity>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24
+  },
+  headingText: {
+    alignSelf: 'center',
+    marginVertical: 26,
+    fontWeight: 'bold',
+    fontSize: 26
+  },
+  textInputStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    maxheight: 100,
+    borderRadius: 20,
+    paddingHorizontal: 16,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 2,
+    shadowOpacity: 0.4
+  }
+});
+
+export default Audio_record;
---
+++ screens/video_screen.js
@@ -0,0 +1,121 @@
+import React, {useState, useEffect,Component} from 'react';
+import VideoPlayer from  "react-native-multiple-video-player"
+import { LogBox } from 'react-native'
+import { View, Image, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, ActivityIndicator, StatusBar } from 'react-native';
+import Video from 'react-native-video';
+import YoutubePlayer from "react-native-youtube-iframe";
+import { useDimensions } from '@react-native-community/hooks'
+import Orientation from 'react-native-orientation-locker'
+import t from '../screens/t.mp4';
+
+LogBox.ignoreAllLogs()
+const DATA = [
+	{
+		uri: "https://rawgit.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4",
+		title: "Elephant Dream"
+	},
+	{
+		uri: "https://rawgit.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4",
+		title: "For Bigger Blazes"
+	},
+]
+const vid = "screens/t.mp4"
+
+const Video_screen = ({navigation, route}) => {
+  const windowWidth = useDimensions().screen.width
+  const windowHeight = useDimensions().screen.height
+  const [playerViewStyle, setPlayerViewStyle] = useState({})
+  const [playerWidth, setPlayerWidth] = useState(0)
+  const [playerHeight, setPlayerHeight] = useState(0)
+  let playerFullScreen = true
+  /** Change the layout when the player goes fullscreen */
+    useEffect(() => {
+  
+      // constants
+      const boxedViewStyle= {
+        position: 'absolute',
+        right: 0,
+        left: 0,
+        zIndex: 1,
+      }
+  
+      const fullscreenViewStyle = {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+        zIndex: 2,
+      }
+  
+      // Do the magic trick to change everything
+        if (playerFullScreen) {
+          Orientation.lockToLandscape() // View horizontal
+          setPlayerViewStyle(fullscreenViewStyle)
+          setPlayerWidth(windowWidth)
+          setPlayerHeight(windowHeight)
+        } else {
+          Orientation.lockToPortrait() // View
+          setPlayerViewStyle(boxedViewStyle)
+          setPlayerWidth(windowWidth)
+          setPlayerHeight(PixelRatio.roundToNearestPixel(windowWidth / (16 / 9)))
+        }
+  
+      return () => {
+        Orientation.lockToPortrait()
+      }
+  
+    }, [playerFullScreen, windowHeight, windowWidth])
+return ( 
+  
+  <View style={styles.container} pointerEvents='none'>
+        {playerFullScreen && <StatusBar hidden={true} />}
+        <YoutubePlayer
+          height={playerHeight}
+          play={true}
+playList={['KyyNxUIS3bc','sui-V3uZH5c','KGUTsC4a-JI','1Koiu2Pjtnk','JZEzhdo8bPo','pdaU89B-2tE','1Koiu2Pjtnk']}
+webViewProps={{
+  injectedJavaScript: `
+    var element = document.getElementsByClassName('container')[0];
+    element.style.position = 'unset';
+    true;
+  `,
+}}
+          initialPlayerParams={{
+            preventFullScreen: true,
+          }}
+          onError={(error) => {
+            console.log('ERROR > MediaPlayerBox > YoutubePlayer: ', error)
+          }}
+          forceAndroidAutoplay={Platform.OS === 'android'}
+        />
+{/* <Video source={{uri: "https://rawgit.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4"}}   // Can be a URL or a local file.
+          // Callback when video cannot be loaded
+       style={styles.video} />
+ <Text>{route.params.res}</Text> */}
+</View>)
+}
+const styles = StyleSheet.create({
+  container: {    
+    justifyContent: 'center',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 400,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Video_screen;
+/* <YouTube
+apiKey='AIzaSyCixVUgTSSeOTaydqDa-AHwfjmOpqcJgpY'
+  videoId="eYnFA86XemA" // The YouTube video ID
+  play // control playback of video with true/false
+   // control whether the video should play in fullscreen or inline
+  style={{ alignSelf: 'stretch', height: 300 }}
+/> */
